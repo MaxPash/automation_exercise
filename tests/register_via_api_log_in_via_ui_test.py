7b116e5442ab473ff4c28dcf45a2ac154691466e
Max Pascov <pascovm@gmail.com>
2026-03-05 15:51:48 +0200

    Update Api + UI test to display name in logs

--- tests/register_via_api_log_in_via_ui_test.py
+++ tests/register_via_api_log_in_via_ui_test.py
@@ -3,26 +3,19 @@ import logging
 from framework.flows.api_user_flows import ApiUserFlows
 
 
-def test_api_create_ui_login_delete(pages, api_context, url):
-    user_api = UserAPI(api_context)
-
-    with open("framework/data/test_signup_data.json") as f:
-        payload = json.load(f)
-
-    email = generate_email()
-    payload["email"] = email
-    password = payload["password"]
+def test_register_via_api_log_in_via_ui(pages, api_user_flows: ApiUserFlows, signup_payload: dict, url):
+    payload = signup_payload
     name = payload["name"]
     # Given: Create user via API
     email, password = api_user_flows.create_unique_user(payload)
-    logging.info("account created with name: %s", name)
+    logging.info("User created with email: %s and name: %s", email, name)
     # When: Login via UI
     pages.base.open_home(url)
     pages.login.open()
     pages.login.log_in(email, password)
     # Then: Verify logged in name
     pages.base.should_show_logged_in_name(name)
-    logging.info("account name verified: %s", name)
+    logging.info("User name verified: %s", name)
     # Finally: Delete user via API
     api_user_flows.delete_user(email, password)
-    logging.info("account deleted")
+    logging.info("User deleted with email: %s", email)
